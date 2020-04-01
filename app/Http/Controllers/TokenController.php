4d3be12d5782abbde9020e45e0007c5795939244
bravofh <fabribraguev@gmail.com>
2020-04-01 15:03:17 -0300

    tymon/jwt-auth was installed and created JWT token generator controller

---
+++ app/Http/Controllers/TokenController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use JWTAuth;
+
+
+class TokenController extends Controller
+{
+
+
+    public function getToken()
+    {
+        
+        //$credentials = request(['email', 'password']);
+        $credentials = array(
+            "email" => "usuario",
+            "password" => "usuario"
+        );
+
+        if (! $token = JWTAuth::attempt($credentials) ) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }        
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer'
+        ], 200);
+        
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -17,19 +17,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route::resource('courses','CourseController');
-//Courses
-Route::get('courses','CourseController@index');
-Route::get('courses/all','CourseController@getAll');
-Route::get('courses/{id}','CourseController@show');
-Route::post('courses','CourseController@store')->name("addCourse");
-Route::put('courses/{id}','CourseController@update')->name("updateCourse");
-Route::delete('courses/{id}','CourseController@destroy')->name("deleteCourse");
-
-
-//Students
-Route::get('students','StudentController@index');
-Route::get('students/all','StudentController@getAll');
-Route::get('students/{id}','StudentController@show');
-Route::post('students','StudentController@store');
-
